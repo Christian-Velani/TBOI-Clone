a733fb5ac5f00ca1d43cac38eb6ee3d2889fad1a
Christian Velani <chris13082000@gmail.com>
2024-01-03 09:26:43 -0300

    Vídeo 1 completo

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    new Rigidbody2D rigidbody;
+    public Text collectedText;
+    public static int collectedAmount = 0;
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rigidbody.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
+        collectedText.text = "Items Collected: " + collectedAmount;
     }
 }
