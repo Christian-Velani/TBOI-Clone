f263d2dd3d7aa08a1d53e876b08b08407e2e20b7
Christian Velani <chris13082000@gmail.com>
2024-01-18 09:18:19 -0300

    Vídeo 11

--- Assets/Scripts/DungeonGeneration/DungeonCrawlerController.cs
+++ Assets/Scripts/DungeonGeneration/DungeonCrawlerController.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+
+public enum Direction
+{
+    top = 0,
+    left = 1,
+    down = 2,
+    right = 3
+}
 
 public class DungeonCrawlerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static List<Vector2Int> positionsVisited = new();
+    private static readonly Dictionary<Direction, Vector2Int> directionMovementMap = new()
     {
-        
-    }
+        {Direction.top, Vector2Int.up},
+        {Direction.down, Vector2Int.down},
+        {Direction.left, Vector2Int.left},
+        {Direction.right, Vector2Int.right}
+    };
 
-    // Update is called once per frame
-    void Update()
+    public static List<Vector2Int> GenerateDungeon(DungeonGenerationData dungeonData)
     {
-        
+        List<DungeonCrawler> dungeonCrawlers = new();
+
+        for (int i = 0; i < dungeonData.numberOfCrawlers; i++)
+        {
+            dungeonCrawlers.Add(new DungeonCrawler(Vector2Int.zero));
+        }
+
+        int iterations = Random.Range(dungeonData.iterationMin, dungeonData.iterationMax);
+
+        for (int i = 0; i < iterations; i++)
+        {
+            foreach (DungeonCrawler dungeonCrawler in dungeonCrawlers)
+            {
+                Vector2Int newPos = dungeonCrawler.Move(directionMovementMap);
+                positionsVisited.Add(newPos);
+            }
+        }
+
+        return positionsVisited;
     }
 }
--- Assets/Scripts/DungeonGeneration/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGeneration/DungeonGenerator.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
+    public DungeonGenerationData dungeonGenerationData;
+    private List<Vector2Int> dungeonRooms;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dungeonRooms = DungeonCrawlerController.GenerateDungeon(dungeonGenerationData);
+        SpawnRooms(dungeonRooms);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnRooms(IEnumerable<Vector2Int> rooms)
     {
-        
+        RoomController.instance.LoadRoom("Start", 0, 0);
+        foreach (Vector2Int roomLocation in rooms)
+        {
+            RoomController.instance.LoadRoom("Empty", roomLocation.x, roomLocation.y);
+        }
     }
 }
