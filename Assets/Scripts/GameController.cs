6252c653ce334ef459bc8f4407ff50907c94cbce
Christian Velani <chris13082000@gmail.com>
2024-01-20 09:07:07 -0300

    Vídeo 14

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,6 +11,9 @@ public class GameController : MonoBehaviour
     public static float MoveSpeed { get; set; } = 5f;
     public static float FireRate { get; set; } = 0.5f;
     public static float BulletSize { get; set; } = 0.5f;
+    private bool bootCollected = false;
+    private bool screwCollected = false;
+    public List<string> collectedNames = new();
     public Text healthText;
     private void Awake()
     {
@@ -55,6 +58,29 @@ public class GameController : MonoBehaviour
     public static void BulletSizeChange(float size)
     {
         BulletSize += size;
+    }
+
+    public void UpdateCollectedItems(CollectionController item)
+    {
+        collectedNames.Add(item.item.name);
+
+        foreach (string itemName in collectedNames)
+        {
+            switch (itemName)
+            {
+                case "Boot":
+                    bootCollected = true;
+                    break;
+                case "Screw":
+                    screwCollected = true;
+                    break;
+            }
+        }
+
+        if (bootCollected && screwCollected)
+        {
+            FireRateChange(0.25f);
+        }
     }
 
     private static void KillPlayer()
