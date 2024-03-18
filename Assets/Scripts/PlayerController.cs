41c67a2e3dd1ea337617721e1ad019915c7d6c74
Christian Velani <chris13082000@gmail.com>
2024-03-18 15:12:32 -0300

    Remoção de código desnecessário e adição de comentários

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,45 +1,38 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    new Rigidbody2D rigidbody;
-    public Text collectedText;
-    public static int collectedAmount = 0;
-    public GameObject bulletPreFab;
-    public float bulletSpeed;
-    private float lastFire;
-    public float fireDelay;
+    public float speed; // Velocidade de movimento do jogador
+    new Rigidbody2D rigidbody; // Corpo do Jogador
+    public GameObject bulletPreFab; // Armazena o pre fab da bala
+    public float bulletSpeed; // Velocidade da bala
+    private float lastFire; // Tempo da última bola
+    public float fireDelay; // Tempo de delay dos tiros
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>(); // Pega o componente rigibody e armazena na variavel
     }
 
     void Update()
     {
-        fireDelay = GameController.FireRate;
-        speed = GameController.MoveSpeed;
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal"); // Armazena o eixo horizontal baseado no Input
+        float vertical = Input.GetAxis("Vertical"); // Armazena o eixo vertical baseado no Input
+        float shootHor = Input.GetAxis("ShootHorizontal"); // Armazena o eixo de tiro horizontal baseado no Input
+        float shootVer = Input.GetAxis("ShootVertical"); // Armazena o eixo de tiro vertical baseado no Input
 
-        float shootHor = Input.GetAxis("ShootHorizontal");
-        float shootVer = Input.GetAxis("ShootVertical");
-        if ((shootHor != 0 || shootVer != 0) && Time.time > lastFire + fireDelay)
+        if ((shootHor != 0 || shootVer != 0) && Time.time > lastFire + fireDelay) // Verifica se qualquer um dos eixos de tiro esta ativo
         {
-            Shoot(shootHor, shootVer);
-            lastFire = Time.time;
+            Shoot(shootHor, shootVer); // Chama a função de atirar
+            lastFire = Time.time; // Atribui o time atual a variável
         }
 
-        rigidbody.velocity = new Vector2(horizontal * speed, vertical * speed);
-        collectedText.text = "Items Collected: " + collectedAmount;
+        rigidbody.velocity = new Vector2(horizontal * speed, vertical * speed); // Deixa a velocidade do jogador baseado no eixo pressionado multiplicado pela velocidade definida
     }
 
     void Shoot(float x, float y)
     {
-        GameObject bullet = Instantiate(bulletPreFab, transform.position, transform.rotation);
-        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
-        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2((x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed, (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed);
+        GameObject bullet = Instantiate(bulletPreFab, transform.position, transform.rotation); // Intancia a bala no local atual do jogador
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0; // Adiciona um RigidBody a bala e registra a gravidade dele para 0
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2((x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed, (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed); // Define a "velocidade" da bala baseada nas variaveis de velocidade e no eixo pressionado
     }
 }
